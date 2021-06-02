2fac44377e4c7528c76d620160465fee56c50fcc
Harrison <harrison.m.martin@gmail.com>
2021-06-03 11:43:43 +1200

    made changes

--- navbar.php
+++ navbar.php
@@ -6,14 +6,12 @@
     </div>
     <nav id="navbar" class="navbar navbar-expand-lg bg-dark navbar-dark">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
         </div>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <div class="navbar-nav">
-
+        <div class="collapse navbar-collapse navbar-dark" id="collapsibleNavbar">
                 <a class="nav-link" href="index.php">Home</a>
                 <a class="nav-link" href="newEvent.php">New task</a>
                 <div class="searchInput ">
@@ -21,8 +19,8 @@
                     <div id="searchResults">
 
                     </div>
-                </div>
-            </div>
+        </div>
+
 
 
 
